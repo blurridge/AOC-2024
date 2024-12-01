4369ff26dc38d4548c07cf6e6463ca80a3e231d2
Zach Riane Machacon <zachriane01@gmail.com>
2024-12-02 01:40:39 +0800

    chore: optimized day_1 code

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,23 +1,21 @@
 # Part 1
-
 left = []
 right = []
 with open("input.txt", "r") as input:
-    lines = input.readlines()
-for line in lines:
-    numbers = line.split()
-    left.append(float(numbers[0]))
-    right.append(float(numbers[1]))
+    for line in input:
+        numbers = line.split()
+        left.append(int(numbers[0]))
+        right.append(int(numbers[1]))
 left.sort()
 right.sort()
 sum = 0
 for numbers in zip(left, right):
     sum += (abs(numbers[0] - numbers[1]))
-print(f"The total difference of the lists are: {sum}")
+print(f"The total difference of the lists are: {sum}") # 2166959
 
 # Part 2
 similarity_score_left = 0
 for number in left:
     instances_right = right.count(number)
     similarity_score_left += (number * instances_right)
-print(f"The total similarity score of the left list is: {similarity_score_left}")
+print(f"The total similarity score of the left list is: {similarity_score_left}") # 23741109
