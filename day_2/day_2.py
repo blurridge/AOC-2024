3c60f79d38991542e6d8c2c4cc237b02e80588c3
Zach Riane Machacon <zachriane01@gmail.com>
2024-12-02 23:57:02 +0800

    finished day 2

---
+++ day_2/day_2.py
@@ -0,0 +1,45 @@
+def check_in_range(num):
+    return num >= 1 and num <= 3
+
+
+def is_safe(report):
+    if report == sorted(report, reverse=True) or report == sorted(report):
+        diffs = [
+            check_in_range(abs(report[i] - report[i + 1]))
+            for i in range(len(report) - 1)
+        ]
+        if all(diffs):
+            return True
+
+
+# Part 1
+reports = []
+with open("input.txt", "r") as input:
+    for line in input:
+        levels = list(map(int, line.split()))
+        reports.append(levels)
+safe = 0
+for report in reports:
+    if is_safe(report):
+        safe += 1
+print(f"The number of safe levels are: {safe}")  # 252
+
+
+# Part 2
+reports = []
+with open("input.txt", "r") as input:
+    for line in input:
+        levels = list(map(int, line.split()))
+        reports.append(levels)
+safe = 0
+for report in reports:
+    if is_safe(report):
+        safe += 1
+    else:
+        for i in range(len(report)):
+            modified_report = report.copy()
+            modified_report.pop(i)
+            if is_safe(modified_report):
+                safe += 1
+                break
+print(f"The number of safe levels with the dampener are: {safe}")  # 324
